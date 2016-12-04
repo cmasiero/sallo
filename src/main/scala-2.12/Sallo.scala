88652b7bbe3c2bc1db635e09b4525110a7af8adc
Cristiano Masiero <c_masiero@yahoo.it>
2016-12-04 19:14:12 +0100

    commands from shell , continue.

--- src/main/scala-2.12/Sallo.scala
+++ src/main/scala-2.12/Sallo.scala
@@ -1,4 +1,10 @@
+import java.io.{File, PrintWriter}
+import java.nio.file.{Files, Paths}
+
 import it.cristiano.sallo.dao.FileDao
+import it.cristiano.sallo.util.CryptoUtils
+
+import scala.io.Source
 
 /**
   * Created by cristiano on 11/21/16.
@@ -7,7 +13,11 @@ import it.cristiano.sallo.dao.FileDao
 
 object Sallo {
 
-//  val EncryptRE = """^\s*([a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+\s*){3}$""".r
+  val DEFAULT_PASS                    = "DEFAULT_PASS"
+  val DECRYPT_FILE                    = "file.csv"
+  val ENCRYPT_FILE                    = "file.csv.enc"
+
+  val scanner = new java.util.Scanner(System.in)
 
   /**
     * <b>command :<b/> usage
@@ -16,38 +26,30 @@ object Sallo {
     */
   def main(args: Array[String]): Unit = {
 
-    val command = args.mkString(" ")
+    val password = Files.exists(Paths.get(ENCRYPT_FILE)) match {
+      case true  => login
+      case false => initializeFile
+    }
 
-    println(s"command:'$command'")
-
-    val scanner = new java.util.Scanner(System.in)
-
-    //  prompt for the user's name
-    print("Enter your name: ")
-
-    // get their input as a String
-    val username = scanner.next()
-
-    // prompt for their age
-    System.out.print("Enter your age: ")
-
-    // get the age as an int
-    val age = scanner.nextInt()
-
-//    println(String.format("%s, your age is %d", username, age));
-    println(s"$username, your age is $age")
+    /*
+        println(s"command:'$command'")
 
 
+        //  prompt for the user's name
+        print("Enter your name: ")
 
+        // get their input as a String
+        val username = scanner.next()
 
-    //
-//    command match {
-//      case ""     => help
-//      case "help" => help
-//      case EncryptRE(command) => encrypt(args.mkString(" "))
-//      case _ => commandNotFound
-//    }
+        // prompt for their age
+        System.out.print("Enter your age: ")
 
+        // get the age as an int
+        val age = scanner.nextInt()
+
+    //    println(String.format("%s, your age is %d", username, age));
+        println(s"$username, your age is $age")
+    */
 
   }
 
@@ -60,6 +62,25 @@ object Sallo {
     println("")
     println("")
     println("")
+  }
+
+  def login: String = {
+    ""
+  }
+
+  def initializeFile: String = {
+    val exampleRecord = "sallo=first-line,attribute=first-attribute"
+    println("*********************************** INITIALIZE ***********************************")
+    println("Sallo needs create your encrypted file, you must insert your pass!")
+    System.out.print("Enter your password: ")
+    val password = scanner.next()
+    val pw = new PrintWriter(new File(DECRYPT_FILE))
+    pw.write(exampleRecord)
+    pw.close
+    CryptoUtils.encrypt(password,DECRYPT_FILE)
+    new File(DECRYPT_FILE).delete()
+    println(s"Your encrypted file has been created, it contains an example records : '$exampleRecord'")
+    password
   }
 
   def commandNotFound: Unit = {
